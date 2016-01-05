2b93313a030edfcc0278515acb38843f08214e11
Andrea Ghidini <ghidosoft@gmail.com>
2016-01-05 14:09:39 +0100

    Imported code from Faucet script

--- src/PaytoshiApi.php
+++ src/PaytoshiApi.php
@@ -1,148 +1,41 @@
 <?php
 
-/**
- * Paytoshi PHP Library
- * 
- * Contact: info@paytoshi.org
- * 
- * @author: Looptribe
- * @link: https://paytoshi.org
- * @package looptribe/paytoshi-library-php
- */
+namespace Looptribe\Paytoshi\Api;
 
-namespace Looptribe\Paytoshi;
+use Buzz\Browser;
 
-class PaytoshiApi
+class PaytoshiApi implements PaytoshiApiInterface
 {
-    const PAYTOSHI_BASE_URL = "http://paytoshi.org/api/v1";
-    
-    private $isCurl;
-    private $verifyPeer;
-    private $timeout;
-    
-    public function __construct($isCurl = true, $verifyPeer = true, $timeout = null)
-    {
-        $this->isCurl = $isCurl;
-        $this->verifyPeer = $verifyPeer;
-        if ($timeout === null)
-        {
-            $socket_timeout = ini_get('default_socket_timeout');
-            $script_timeout = ini_get('max_execution_time');
-            $timeout = min($script_timeout / 2, $socket_timeout);
-        }
-        $this->timeout = $timeout;
-    }
-    
-    private function getBaseUrl()
-    {
-        return self::PAYTOSHI_BASE_URL;
-    }
-    
-    private function isCurl()
-    {
-        return $this->isCurl && function_exists('curl_version');
-    }
-    
-    protected function getUrl($api, $params = array())
-    {
-        $url = $this->getBaseUrl() . '/' . $api;
-        if (!empty($params))
-            $url .= '?' . http_build_query($params);
-        return $url;
-    }
-    
-    private function runCurl($url, $data)
-    {
-        $ch = curl_init($url);
-        curl_setopt_array($ch, array(
-            CURLOPT_SSL_VERIFYPEER => $this->verifyPeer,
-            CURLOPT_TIMEOUT => (int)$this->timeout,
-            CURLOPT_RETURNTRANSFER => true
-        ));
-        
-        if (!empty($data))
-        {
-            curl_setopt_array($ch, array(
-                CURLOPT_POST => TRUE,
-                CURLOPT_POSTFIELDS => $data
-            ));
-        }
+    /** @var Browser */
+    private $browser;
 
-        $response = curl_exec($ch);
-        curl_close($ch);
+    /** @var string */
+    private $baseUrl;
 
-        return $response;
-    }
-    
-    private function runFileGetContents($url, $data)
+    public function __construct(Browser $browser, $baseUrl)
     {
-        $opts = array(
-            'ssl' => array('verify_peer' => $this->verifyPeer)
+        $this->browser = $browser;
+        $this->baseUrl = $baseUrl;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function send($apikey, $address, $amount, $ip, $referral = false)
+    {
+        $url = $this->baseUrl . 'faucet/send?' . http_build_query(array('apikey' => $apikey));
+        $headers = array(
+            'Connection' => 'close',
         );
-        if (!empty($data))
-        {
-            $opts['http'] = array(
-                'method' => 'POST',
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-                'content' => $data
-            );
-        }
-        else
-            $opts['http'] = array('method' => 'GET');
-        
-        $opts['http']['timeout'] = $this->timeout;
-        
-        $context = stream_context_create($opts);
-        $response = @file_get_contents($url, false, $context);
-        return $response;
-    }
-    
-    private function run($url, $data = array())
-    {
-        if ($this->isCurl())
-            $response = $this->runCurl($url, $data);
-        else
-            $response = $this->runFileGetContents($url, $data);
-        
-        return $response;
-    }
-    
-    protected function get($api, $params = array())
-    {
-        $url = $this->getUrl($api, $params);
-        
-        $response = $this->run($url);
-        if (!$response)
-            return null;
-        
-        return @json_decode($response, true);
-    }
-    
-    protected function post($api, $postData = array(), $params = array())
-    {
-        $url = $this->getUrl($api, $params);
-        $data = http_build_query($postData);
-        
-        $response = $this->run($url, $data);
-        if (!$response)
-            return null;
-
-        return @json_decode($response, true);
-    }
-    
-    public function faucetSend($faucetApikey, $address, $amount, $ip, $referral = false)
-    {
-        return $this->post('faucet/send', array(
+        $data = http_build_query(array(
             'address' => $address,
             'amount' => $amount,
-            'ip' => $ip,
             'referral' => $referral,
-            ), array('apikey' => $faucetApikey));
-    }
+            'ip' => $ip
+        ));
 
-    public function faucetBalance($faucetApikey)
-    {
-        return $this->get('faucet/balance',
-            array('apikey' => $faucetApikey));
+        $response = $this->browser->post($url, $headers, $data);
+
+        return new SendApiResponse($response);
     }
 }
